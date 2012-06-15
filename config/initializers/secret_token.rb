7cc816151a84a7cd4207dede645215bb8fdef04b
Yerlan Kussainov <lagox.mail@gmail.com>
2012-06-16 02:16:05 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Astana::Application.config.secret_token = '6748ecc32e887907e7f738114515b7e702c4516be9c54528bf08d6e562803dda22850f7e71e7e6e34a6b60ae33bfe5bbf38817634beec36bc14e77fc9aa8e023'
